19e2ce54615284af22524b8c556f328b5a74f365
anucha-tk <anuchataowkaen@gmail.com>
2026-02-23 15:32:58 +0700

    feat(tailwind): integrate Tailwind CSS and Vite into Nuxt configuration

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold underline text-amber-300">Hello World</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    // @ts-expect-error tailwindcss is a valid plugin
+    plugins: [tailwindcss()],
+  },
 });
